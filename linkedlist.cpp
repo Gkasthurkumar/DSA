2101cafed3037c0b27402ea29381c9c06213e1b4
Gkasthurkumar <kasthurchintu@gmail.com>
2026-04-24 15:58:46 +0530

    linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -31,6 +31,62 @@ void push_back(node* &head,node* &tail,int val){
         tail=temp;
     }  
 }
+void pop_front(node* &head,node* tail){
+    node* temp=new node();
+    if(head==NULL)
+    cout<<"empty list"<<endl;
+    
+    temp=head;
+    head=head->next;
+    temp->next=NULL;
+    delete temp;
+    
+    
+}
+
+void pop_back(node* &head,node* &tail){
+    node* temp=new node();
+    if(head==NULL)
+    return;
+    temp=head;
+    while(temp->next!=tail)
+    temp=temp->next;
+    
+    temp->next=NULL;
+    
+    tail=temp;
+}
+
+void print(node* &head,node* &tail){
+    node* temp=head;
+     while(temp!=NULL){
+        cout<<temp->data<<"\t";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+void insert(node* head,node* tail ,int val,int pos){
+    if(pos<0){
+    cout<<"invalid position"<<endl;
+    return;
+    }
+    if(pos==1){
+    push_front(head,tail,val);
+    return;
+    }
+    node* newnode=new node();
+    newnode->data=val;
+    
+    node* temp=head;
+    for(int i=0;i<pos-1;i++)
+    temp=temp->next;
+    
+    newnode->next=temp->next;
+    temp->next=newnode;
+
+}
+
 int main(){
     node* head=NULL;
     node* tail=NULL;
@@ -38,18 +94,16 @@ int main(){
     push_front(head,tail,10);
     push_front(head,tail,20);
     push_front(head,tail,30);
-    cout<<"Head element: "<<head->data<<endl;
-    cout<<"Tail element: "<<tail->data<<endl;
+    print(head,tail);
     push_back(head,tail,10);
     push_back(head,tail,20);
     push_back(head,tail,30);
+    print(head,tail);
+    pop_front(head,tail);
+    print(head,tail);
+    pop_back(head,tail);
+    print(head,tail);
     
-    cout<<"Head element: "<<head->data<<endl;
-    cout<<"Tail element: "<<tail->data<<endl;
-
-    node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<"\t";
-        temp=temp->next;
-    }
+    insert(head,tail,50,3);
+    print(head,tail);
 }
